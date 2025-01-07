3d841330b77d604333287dc73ecbccf6a6aba9cc
Jesse Lansner <jlansner@gmail.com>
2025-01-07 12:36:49 -0500

    Increase recently used count

--- includes/definitions.php
+++ includes/definitions.php
@@ -2,7 +2,7 @@
 
 include( "passwords/passwords.php" );
 
-define( "RECENTLY_USED_COUNT", 10 );
+define( "RECENTLY_USED_COUNT", 15 );
 define( "BASE_SQL", "SELECT translations.id as translationId, translations.translation, translations.translation_eng, translations.not_translated, translations.transliteration,
     book.title, book.year, book.url, book.title_transliteration,
     author.first_name, author.middle_name, author.last_name, author.additional_name, author.name_transliteration,
